ebad1b9898a074b00f4ab6e94ff0a2c4cdb958ac
sadrayan <sadrayan@gmail.com>
2020-10-04 21:33:51 -0400

    kubernetes udpates

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=sadrayan/housing-price-project-4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --image "$dockerpath":1.0 --port 80 housing-price-project-4 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward --address 0.0.0.0 housing-price-project-4 8080:80
